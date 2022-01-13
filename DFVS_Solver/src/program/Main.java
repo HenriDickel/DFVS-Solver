14e0b9de7b729ec866e071ee70b5074e14782f88
lennartuhrmacher <lennartuhrmacher@gmail.com>
2022-01-13 14:43:21 +0100

    Presentation 3

--- DFVS_Solver/src/program/Main.java
+++ DFVS_Solver/src/program/Main.java
@@ -52,24 +52,26 @@ public class Main {
     }
 
     private static void testLowerBoundPerformance(List<GraphFile> files) {
+        int count = 0;
+        long millisAgg = 0;
         for(GraphFile file: files) {
             Instance instance = InstanceCreator.createFromFile(file);
+            if(instance.OPTIMAL_K == -1) continue;
             Graph graph = instance.subGraphs.get(0);
             List<Integer> reduceS = Reduction.applyRules(graph, true);
+            int remainingK = instance.OPTIMAL_K - reduceS.size();
+            if(remainingK <= 0) continue;
             // Compute packing
             long startTime = System.nanoTime();
             CyclePacking packing = new CyclePacking(graph.copy());
             int lowerBound = packing.size();
             long millis = (System.nanoTime() - startTime) / 1000000;
 
-            // Compute slow packing
-            long slowStartTime = System.nanoTime();
-            SlowPacking slowPacking = new SlowPacking(graph.copy());
-            int slowLowerBound = slowPacking.size();
-            long slowMillis = (System.nanoTime() - slowStartTime) / 1000000;
-
-            Log.debugLog(instance.NAME, "Computed lower bound = (" + lowerBound + " / " + slowLowerBound + ") in (" + millis + " / " + slowMillis + ") ms");
+            Log.debugLog(instance.NAME, "Computed lower bound = " + lowerBound + " in " + millis + " ms");
+            count++;
+            millisAgg += millis;
         }
+        Log.debugLog("Overall (" + count + ")", "Average millis: " + millisAgg / count);
     }
 
     private static void testLowerBoundQuality(List<GraphFile> files) {
@@ -79,16 +81,17 @@ public class Main {
         int count = 0;
         for(GraphFile file: files) {
             Instance instance = InstanceCreator.createFromFile(file);
+            if(instance.OPTIMAL_K == -1) continue;
             Graph graph = instance.subGraphs.get(0);
             // Apply reduction rules and calculate remaining k for lower bounds
             List<Integer> reduceS = Reduction.applyRules(graph, true);
             int remainingK = instance.OPTIMAL_K - reduceS.size();
 
-            //CyclePacking packing = new CyclePacking(graph.copy());
-            //int lowerBound = packing.size();
-
-            PackingManager packing = new PackingManager(graph.copy());
+            CyclePacking packing = new CyclePacking(graph.copy());
             int lowerBound = packing.size();
+
+            //PackingManager packing = new PackingManager(graph.copy());
+            //int lowerBound = packing.size();
 
             if(remainingK > 0) {
                 lowerBoundAgg += lowerBound;
@@ -99,8 +102,8 @@ public class Main {
             Log.debugLog(instance.NAME, "Lower bound: " + lowerBound + " / " + remainingK);
         }
         float quality = qualityAgg / count;
-        Log.debugLog("Overall", "Average lower bound quality per instance: " + quality);
+        Log.debugLog("Overall (" + count + ")", "Average lower bound quality per instance: " + quality);
         float overallLowerBoundK = (float) lowerBoundAgg / remainingKAgg;
-        Log.debugLog("Overall", "Overall lower bound quality: " + overallLowerBoundK);
+        Log.debugLog("Overall (" + count + ")", "Overall lower bound quality: " + overallLowerBoundK);
     }
 }
